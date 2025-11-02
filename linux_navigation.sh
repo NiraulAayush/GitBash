c7eefb2e85fb6c8b7d4b8756e4650ae9a846fcd3
Aayush Niraula <aayush.niraula@ankaek.com>
2025-11-02 14:43:13 +0545

    add:Linux Foundation

---
+++ linux_navigation.sh
@@ -0,0 +1,23 @@
+1. Find the working location
+- Print the working directory: pwd
+
+2. Changing the directory
+- Navigate to the Root: cd [directory_name]
+- Navigate to the System Directory: cd bin
+- Go Up One Level: cd ..
+- Return home: cd ~
+
+3. Listing Files and Directories
+- Simple List: ls
+- Long List with details like file permissions, ownership, size: ls -l
+- List with hidden files: ls -a
+- Combined Options: ls -al
+
+4. Getting Help
+- Manual pages: man ls
+- Keyword Search: apropos help, apropos permissions
+
+Note:
+- Tilde (~) for home directory
+- Single dot (.) for current directory
+- Double dot (..) for the directory (one level up)
