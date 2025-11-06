c3e9c33d161fb0d641492e753ce1face1cd0dd55
Aayush Niraula <aayush.niraula@ankaek.com>
2025-11-06 16:30:50 +0545

    add: io redirection

---
+++ io_redirection/redirection_io.sh
@@ -0,0 +1,15 @@
+Overwrite
+echo "Line 1" > output.txt
+
+Append
+echo "Line 2" >> output.txt
+
+Exercise 1
+echo "Start of the log file" > log.txt
+
+Exercise 2
+head -n 2 /etc/passwd >> log.txt
+
+Exercise 3
+cat log.txt
+
