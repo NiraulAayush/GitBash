6c948df5595504a00a47a0d665cb384eb6793cb8
Aayush Niraula <aayush.niraula@ankaek.com>
2025-11-06 15:44:41 +0545

    add: Text editor linux basics

---
+++ nano_editor/myscript.sh
@@ -0,0 +1,19 @@
+Ctrl + K = Cut a low
+
+Arrow keys = Navigation
+
+Ctrl + U = Paste Line
+
+Ctrl + W = Search text
+
+Ctrl + O = Save file
+
+Ctrl + X = Exit Editor
+
+Ctrl + G = Help
+
+**Vim Editor**
+
+- Normal:  Esc
+- Insert: i or a
+- Visual : v
